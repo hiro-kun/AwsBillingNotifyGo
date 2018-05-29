216e7d0a3387c4ca619c0240090bc6fa1931437f
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-05-29 22:05:12 +0900

    fix 環境変数の設定を追加

--- conf/env.go
+++ conf/env.go
@@ -3,4 +3,5 @@ package conf
 type Config struct {
     AWS_ACCESS_KEY        string `required:"true"`
     AWS_SECRET_ACCESS_KEY string `required:"true"`
+    LINE_NOTIFY_API_TOKEN string `required:"true"`
 }
