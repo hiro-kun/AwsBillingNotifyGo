43d7add7538270282ca6b7afdde0bfb5f84a2c4f
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-05-29 21:51:07 +0900

    init 定数設定ファイル

---
+++ conf/const.go
@@ -0,0 +1,9 @@
+package conf
+
+const (
+	Region         = "us-east-1"
+	Namespace      = "AWS/Billing"
+	MetricName     = "EstimatedCharges"
+	DimensionName  = "Currency"
+	DimensionValue = "USD"
+)
